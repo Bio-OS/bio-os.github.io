dfc944ec7391d8fc3a26bf6050e77b065d5892a1
yulitao <yulitao@bytedance.com>
2024-07-17 10:58:25 +0800

    feat: 增加培训视频内容

--- src/ActivityHelp/TabVideo.tsx
+++ src/ActivityHelp/TabVideo.tsx
@@ -5,11 +5,25 @@ export default function TabVideo() {
     <>
       <div className="font-medium text-lg mb-6">💻 培训视频</div>
 
-      <div>
-        {/* <Link href="https://bilibili.com/course/1499" target="_blank">
-          点击前往培训视频
-        </Link> */}
-        敬请期待
+      <div className="flex flex-col items-start">
+        <Link
+          href="https://meetings.larkoffice.com/s/1k4eiw161jytg"
+          target="_blank"
+        >
+          点击前往培训视频 第一期
+        </Link>
+        <Link
+          href="https://meetings.larkoffice.com/s/1k4hrd6vbtmv7"
+          target="_blank"
+        >
+          点击前往培训视频 第二期
+        </Link>
+        <Link
+          href="https://meetings.larkoffice.com/s/1k4m36683h0jl"
+          target="_blank"
+        >
+          点击前往培训视频 第三期
+        </Link>
       </div>
     </>
   );
